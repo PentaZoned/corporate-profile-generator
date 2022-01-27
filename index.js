85ae07123a1f652debb7e60684d4171e3e3b77c7
Bradley Le <brabrele@gmail.com>
2022-01-27 03:01:53 -0800

    added list of types of members to add, added information questions for engineer option, also looped the engineer function to ask for next member until selected to stop adding members

--- index.js
+++ index.js
@@ -23,7 +23,68 @@ inquirer
             message: "Enter the manager's office number.",
             name: 'managerOfficeNum',
         },
+        {
+            type: 'list',
+            message: "Which type of employee would you like to add?",
+            choices: ["Engineer", "Intern", "I don't want to add any more employees."],
+            name: 'addEmployee',
+        },
     ])
     .then((data) => {
         console.log(data);
+
+        if(data.addEmployee === "Engineer") {
+            console.log("1");
+            addEngineer();
+        } else if(data.addEmployee === "Intern") {
+            console.log("2");
+        } else {
+            console.log("3");
+        }
+
     })
+
+function addEngineer() {
+
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: "Enter the engineer's name.",
+            name: 'engineerName',
+        },
+        {
+            type: 'input',
+            message: "Enter the engineer's ID.",
+            name: 'engineerID',
+        },
+        {
+            type: 'input',
+            message: "Enter the engineer's email address.",
+            name: 'engineerEmail',
+        },
+        {
+            type: 'input',
+            message: "Enter the engineer's Github username.",
+            name: 'engineerGithub',
+        },
+        {
+            type: 'list',
+            message: "Which type of employee would you like to add?",
+            choices: ["Engineer", "Intern", "I don't want to add any more employees."],
+            name: 'addEmployee',
+        },
+    ])
+    .then((data) => {
+        console.log(data);
+
+        if(data.addEmployee === "Engineer") {
+            console.log("1");
+            addEngineer();
+        } else if(data.addEmployee === "Intern") {
+            console.log("2");
+        } else {
+            console.log("3");
+        }
+    })
+}
