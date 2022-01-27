dbac2f186f21881d1cfb3a135e54ffce9365d418
Bradley Le <brabrele@gmail.com>
2022-01-26 18:56:22 -0800

    added test cases for employee.test.js

---
+++ __test__/employee.test.js
@@ -0,0 +1,35 @@
+const Employee = require("../lib/employee");
+
+describe('employee', () => {
+    it('getName returns the name of the current employee instace', () => {
+
+        let employee1 = new Employee("Emily", "10", "emily@gmail.com");
+        let currentName = employee1.getName();
+        // Assert
+        expect(currentName).toBe("Emily");;
+    })
+
+    it('getId returns the Id of the current employee instance', () => {
+
+        let employee1 = new Employee("Emily", "10", "emily@gmail.com");
+        let currentId = employee1.getId();
+        // Assert
+        expect(currentId).toBe("10");;
+    });
+
+    it('getEmail returns the email of the current employee instance', () => {
+
+        let employee1 = new Employee("Emily", "10", "emily@gmail.com");
+        let currentEmail = employee1.getEmail();
+        // Assert
+        expect(currentEmail).toBe("emily@gmail.com");;
+    });
+
+    it('getEmail returns the email of the current employee instance', () => {
+
+        let employee1 = new Employee("Emily", "10", "emily@gmail.com");
+        let currentRole = employee1.getRole();
+        // Assert
+        expect(currentRole).toBe("Employee");
+    });
+});
