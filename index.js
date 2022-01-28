6eb2ef06f96f53d1ee72ccb73ecee4cb11b8f3a2
Bradley Le <brabrele@gmail.com>
2022-01-27 19:30:04 -0800

    corrected variable names to make the application run properly

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ inquirer
     .then((data) => {
         // Assigns the user inputs to a new instance of the Manager class
         var newManager = new Manager(data.managerName, data.managerID,
-            data.managerEmail, managerOfficeNum);
+            data.managerEmail, data.managerOfficeNum);
 
         // Calls on the methods of the Manager class and assigns them to a usuable variable
         let uniqueRole = newManager.getRole();
@@ -74,12 +74,11 @@ inquirer
         // Executes the function depending on user input
         if(data.addEmployee === "Engineer") {
             addEngineer();
-        }
-
-        if(data.addEmployee === "Intern") {
+        } else if(data.addEmployee === "Intern") {
             addIntern();
+        } else {
+            generateHTML();
         }
-
     })
 
 function addEngineer() {
@@ -118,11 +117,11 @@ function addEngineer() {
         var newEngineer = new Engineer(data.engineerName, data.engineerID,
                                     data.engineerEmail, data.engineerGithub);
 
-        let uniqueRole = newIntern.getRole();
-        let uniqueName = newIntern.getName();
-        let uniqueID = newIntern.getId();
-        let uniqueEmail = newIntern.getEmail();
-        let uniqueGithub = newIntern.getGithub();
+        let uniqueRole = newEngineer.getRole();
+        let uniqueName = newEngineer.getName();
+        let uniqueID = newEngineer.getId();
+        let uniqueEmail = newEngineer.getEmail();
+        let uniqueGithub = newEngineer.getGithub();
         
         var engineerStr = `
         <div class="card m-5 mt-4" style="width: 18rem;">
@@ -144,10 +143,10 @@ function addEngineer() {
 
         if(data.addEmployee === "Engineer") {
             addEngineer();
-        }
-        
-        if(data.addEmployee === "Intern") {
+        } else if(data.addEmployee === "Intern") {
             addIntern();
+        } else {
+            generateHTML();
         }
     })
 }
@@ -203,21 +202,53 @@ function addIntern() {
                 <li class="list-group-item">Name: ${uniqueName}</li>
                 <li class="list-group-item">ID: ${uniqueID}</li>
                 <li class="list-group-item">Email: ${uniqueEmail}</li>
-                <li class="list-group-item">Github: ${uniqueSchool}</li>
+                <li class="list-group-item">School: ${uniqueSchool}</li>
             </ul>
         </div>
   
     `;
         templateStr += internStr;
-        
+
         console.log(newIntern);
 
         if(data.addEmployee === "Engineer") {
             addEngineer();
-        }
-        
-        if(data.addEmployee === "Intern") {
+        } else if(data.addEmployee === "Intern") {
             addIntern();
+        } else {
+            generateHTML();
         }
     })
+}
+
+function generateHTML() {
+    const upperString = `<!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </head>
+    
+    <body>
+        <div class="d-flex justify-content-center p-5 bg-primary text-white bg-gradient">
+            <h1>Team Members</h1>
+        </div>
+    
+        <div class="d-flex flex-row flex-wrap my-5 justify-content-evenly">`;
+
+    const lowerString = `</div>
+    </body>
+    
+    </html>`;
+
+    let newString = upperString + templateStr + lowerString;
+
+    fs.writeFile('./dist/index.html', newString, (err) =>
+        err ? console.log(err) : console.log("Success!")
+    );
 }
